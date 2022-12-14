60885d2c9a900c6defa1fa7838fd899e21f5142e
Iko Prastya <ikoprastyaaaa@gmail.com>
2022-12-14 20:01:56 +0800

    success register validate

--- jumpstart/app/Http/Livewire/Pages/Registration.php
+++ jumpstart/app/Http/Livewire/Pages/Registration.php
@@ -2,10 +2,38 @@
 
 namespace App\Http\Livewire\Pages;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Registration extends Component
 {
+
+    public $name;
+    public $email;
+    public $password;
+    public $confirm_password;
+
+    public function store(){
+        $this->validate([
+            'name'      => 'required',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|confirmed',
+            'confirm_password'  => 'required|same:password'
+        ]);
+
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->bcrypt($this->password)
+        ]);
+
+        if ($user) {
+            session()->flash('success', 'Success Registration');
+            return redirect()->route('login');
+        }
+
+    }
+
     public function render()
     {
         return view('livewire.pages.registration')->layout('layouts.base', ['title' => 'Jumpstart - Registration']);
--- jumpstart/resources/views/livewire/pages/registration.blade.php
+++ jumpstart/resources/views/livewire/pages/registration.blade.php
@@ -14,26 +14,46 @@
                     <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Create and account
                     </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+                    <form class="space-y-4 md:space-y-6" wire:submit.prevent="store">
                         <div>
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required="">
+                            <input type="text" wire:model="name" name="name" id="name" class=" @error('name') border border-red-400 @enderror bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" >
+                            @error('name')
+                                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                            <input type="email"wire:model="email"  name="email" id="email" class="@error('email') border border-red-400 @enderror bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" >
+                            @error('email')
+                                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <label for="password" class="@error('password') error @enderror block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" wire:model="password" name="password" id="password" placeholder="••••••••" class="@error('password') border border-red-400 @enderror bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                            @error('password')
+                                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                         <div>
-                            <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                            <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <label for="confirm-password" class="@error('confirm_password') error @enderror block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                            <input type="confirm-password" wire:model="confirm_password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="@error('confirm_password') border border-red-400 @enderror bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                            @error('confirm_password')
+                                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                         <div class="flex items-start">
                             <div class="flex items-center h-5">
-                              <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                              <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" >
                             </div>
                             <div class="ml-3 text-sm">
                               <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
@@ -41,7 +61,7 @@
                         </div>
                         <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Already have an account? <a href="{{ url('/login') }}" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                            Already have an account? <Button type="submit" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</button>
                         </p>
                     </form>
                 </div>
