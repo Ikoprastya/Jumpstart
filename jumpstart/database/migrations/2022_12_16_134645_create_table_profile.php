626bc46e01efa4f2beca23bca0b83f36f68f7975
Iko Prastya <ikoprastyaaaa@gmail.com>
2022-12-16 21:53:57 +0800

    Create 2022_12_16_134645_create_table_profile.php

---
+++ jumpstart/database/migrations/2022_12_16_134645_create_table_profile.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->string('gender');
+            $table->integer('phone');
+            $table->string('country');
+            $table->integer('amount');
+            $table->date('birthday');
+            $table->longText('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_profile');
+    }
+};
